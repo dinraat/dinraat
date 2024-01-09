5467735fb47d774575ee98f3c126263f7821975b
dinraat <156084447+dinraat@users.noreply.github.com>
2024-01-09 19:00:19 +0530

    p1.py

--- p1.py
+++ p1.py
@@ -1,105 +1,70 @@
-def aStarAlgo(start_node, stop_node):
+class Graph:
+    def __init__(self, adjac_lis):
+        self.adjac_lis = adjac_lis
     
+    def get_neighbors(self, v):
+        return self.adjac_lis[v]
+    
+    def h(self, n):
+        H = {
+            'A': 1,
+            'B': 1,
+            'C': 1,
+            'D': 1,
+            'F': 1,
+        }
+        return H[n]
+    
+    def a_star_algorithm(self, start, stop):
+        open_lst = set([start])
+        closed_lst = set([])
+        cost = {}
+        cost[start] = 0
+        adj_list = {}
+        adj_list[start] = start
 
-    open_set = set(start_node) 
-    closed_set = set()
-    g = {}
-    parents = {}
-    g[start_node] = 0
-    parents[start_node] = start_node
-
-    while len(open_set) > 0 :
-        n = None
-        
-        for v in open_set: 
-            if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
-                n = v 
-
-        if n == stop_node or Graph_nodes[n] == None:
-            pass
-        else:
-            for (m, weight) in get_neighbors(n):
-                if m not in open_set and m not in closed_set:
-                    open_set.add(m)     
-                    parents[m] = n      
-                    g[m] = g[n] + weight 
-
-
+        while len(open_lst) > 0:
+            n = None
+            for v in open_lst:
+                if n == None or cost[v] + self.h(v) < cost[n] + self.h(n):
+                    n = v
             
+            if n == None:
+                print('Path does not exist!')
+                return None
+            
+            if n == stop:
+                reconst_path = []
+                while adj_list[n] != n:
+                    reconst_path.append(n)
+                    n = adj_list[n]
+                reconst_path.append(start)
+                reconst_path.reverse()
+                print('Path found: {}'.format(reconst_path))
+                return reconst_path
+            
+            for (m, weight) in self.get_neighbors(n):
+                if m not in open_lst and m not in closed_lst:
+                    open_lst.add(m)
+                    adj_list[m] = n
+                    cost[m] = cost[n] + weight
                 else:
-                    if g[m] > g[n] + weight:
-                    
-                        g[m] = g[n] + weight
-                    
-                        parents[m] = n
-
-                  
-                        if m in closed_set:
-                            closed_set.remove(m)
-                            open_set.add(m)
-
-        if n == None:
-            print('Path does not exist!')
-            return None
-
+                    if cost[m] > cost[n] + weight:
+                        cost[m] = cost[n] + weight
+                        adj_list[m] = n
+            
+            open_lst.remove(n)
+            closed_lst.add(n)
         
-        if n == stop_node:
-            path = []
-
-            while parents[n] != n:
-                path.append(n)
-                n = parents[n]
-
-            path.append(start_node)
-
-            path.reverse()
-
-            print('Path found: {}'.format(path))
-            return path
-
-
-        
-        open_set.remove(n)
-        closed_set.add(n) 
-
-    print('Path does not exist!')
-    return None
-
-
-def get_neighbors(v):
-    if v in Graph_nodes:
-        return Graph_nodes[v]
-    else:
+        print('Path does not exist!')
         return None
 
-def heuristic(n):
-    H_dist = {
-        'A': 10,
-        'B': 8,
-        'C': 5,
-        'D': 7,
-        'E': 3,
-        'F': 6,
-        'G': 5,
-        'H': 3,
-        'I': 1,
-        'J': 0
-    }
-
-    return H_dist[n]
-
-
-Graph_nodes = {
-    
-    'A': [('B', 6), ('F', 3)],
-    'B': [('C', 3), ('D', 2)],
-    'C': [('D', 1), ('E', 5)],
-    'D': [('C', 1), ('E', 8)],
-    'E': [('I', 5), ('J', 5)],
-    'F': [('G', 1),('H', 7)] ,
-    'G': [('I', 3)],
-    'H': [('I', 2)],
-    'I': [('E', 5), ('J', 3)],
-
+adjac_lis = {
+    'A': [('B', 1), ('C', 3), ('D', 7)],
+    'B': [('D', 5), ('F', 2)],
+    'C': [('D', 12)],
+    'F': [('D', 1)]
 }
-aStarAlgo('A', 'J')
+
+graph1 = Graph(adjac_lis)
+graph1.a_star_algorithm('A', 'D')
